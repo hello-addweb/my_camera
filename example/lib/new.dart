48c104a56bfcf3b1ac21a781ced6bcc8768dd6da
ronakaddweb <ronak.j.addweb@gmail.com>
2023-05-11 11:19:12 +0530

    update camera code

--- example/lib/new.dart
+++ example/lib/new.dart
@@ -73,7 +73,7 @@ class _HomeState extends State<Home> {
         items: _getCameras(),
         onChanged: (value) {
           _previewOcr = null;
-          setState(() => _cameraOcr = value!);
+          setState(() => _cameraOcr != value);
         },
         value: _cameraOcr,
       ),
